9781a3bddc200065ae64e77b2d428b937cbe54f8
ijlal07 <ijlal.nasir@gmail.com>
2018-09-03 15:09:59 +0500

    Removed model from layouts

--- ERP_SupplyChain/Controllers/DashboardController.cs
+++ ERP_SupplyChain/Controllers/DashboardController.cs
@@ -8,11 +8,12 @@ using System.Web.Mvc;
 
 namespace ERP_SupplyChain.Controllers
 {
-<<<<<<< HEAD
     public class DashboardController : Controller
     {
         AccountLogic Accounts = new AccountLogic();
+
         List<AuthenticatedUser> User = new List<AuthenticatedUser>();
+
         //
         // GET: /Dashboard/Supply Chain
         public ActionResult SC_Index()
@@ -30,12 +31,12 @@ namespace ERP_SupplyChain.Controllers
 
         // GET: /Dashboard/Admin
         public ActionResult Admin()
-        {   
-            if(Session["Userinfo"] != null)
-              {
-                  User = Session["Userinfo"] as List<AuthenticatedUser>;
-                  return View(User);
-              }
+        {
+            if (Session["Userinfo"] != null)
+            {
+                User = Session["Userinfo"] as List<AuthenticatedUser>;
+                return View(User);
+            }
             else
             {
                 return Redirect("/Accounts/Login");
@@ -98,39 +99,6 @@ namespace ERP_SupplyChain.Controllers
                 return Redirect("/Accounts/Login");
             }
         }
-       
-=======
-	public class DashboardController : Controller
-	{
-		AccountLogic Accounts = new AccountLogic();
-		List<AuthenticatedUser> User = new List<AuthenticatedUser>();
-		//
-		// GET: /Dashboard/
-		public ActionResult Index()
-		{
-			if (Session["Userinfo"] != null)
-			{
-				User = Session["Userinfo"] as List<AuthenticatedUser>;
-				return View(User);
-			}
-			else
-			{
-				return Redirect("/Accounts/Login");
-			}
-		}
-		public ActionResult Admin()
-		{   
-			if(Session["Userinfo"] != null)
-			  {
-				  User = Session["Userinfo"] as List<AuthenticatedUser>;
-				  return View(User);
-			  }
-			else
-			{
-				return Redirect("/Accounts/Login");
-			}
-		}
-	   
->>>>>>> db6666cc8e62d46f7539fe4b2e985ff139f19fb2
-	}
+
+    }
 }
