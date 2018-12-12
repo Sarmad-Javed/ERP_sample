e1dd94c26eb568dca1b2d1e7999f17119f553fc1
Sarmad-Javed <37639311+Sarmad-Javed@users.noreply.github.com>
2018-12-12 07:39:34 +0500

    FrontDesk
    Bill Display

---
+++ DataAccessLayer/DAL_Logic/FrontDesk/FrontDeskDAL.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPEntities;
+using ERPEntities.Models;
+
+namespace DataAccessLayer.DAL_Logic.FrontDesk
+{
+    public class FrontDeskDAL
+    {
+         List<AppointmentDetails> AppList = new List<AppointmentDetails>();
+
+         public List<AppointmentDetails> getScheduledAppointmentDetails()
+         {
+             ERP1DataContext dc = new ERP1DataContext();
+             var result = from app in dc.Appointments
+                          join doc in dc.Doctors on app.DoctorID equals doc.DoctorID
+                          join p in dc.Patients on app.PatientID equals p.PatientID
+                          where app.Status == "Scheduled"
+                          select new
+                          {
+                              AppointmentID = app.AppointmentID,
+                              DoctorID = doc.DoctorID,
+                              Department = doc.Designation,
+                              DoctorName = doc.FirstName + doc.LastName,
+                              DoctorImage = doc.Image,
+                              DoctorEmail = doc.DoctorEmail,
+                              PatientID = p.PatientID,
+                              PatientName = p.PatientName,
+                              PatientEmail = p.PatientEmail,
+                              PatientContact = p.PatientContact,
+                              PatientImage = p.PatientImage,
+                              Date = app.Date,
+                              TimeSlot = app.TimeSlot,
+                              Status = app.Status
+
+                          };
+
+
+             if (result != null)
+             {
+                 foreach (var v in result)
+                 {
+
+                     AppointmentDetails details = new AppointmentDetails();
+                     details.AppointmentID = v.AppointmentID;
+                     details.DoctorID = v.DoctorID;
+                     details.Designation = v.Department;
+                     details.DoctorName = v.DoctorName;
+                     details.DoctorImage = v.DoctorImage;
+                     details.DoctorEmail = v.DoctorEmail;
+                     details.PatientID = v.PatientID;
+                     details.PatientName = v.PatientName;
+                     details.PatientEmail = v.PatientEmail;
+                     details.PatientContact = v.PatientContact;
+                     details.PatientImage = v.PatientImage;
+                     details.Date = v.Date.ToString();
+                     details.TimeSlot = v.TimeSlot;
+                     AppList.Add(details);
+
+                 }
+                 return AppList;
+             }
+             return AppList;
+         }
+         public List<AppointmentDetails> getReferedAppointmentDetails()
+         {
+             ERP1DataContext dc = new ERP1DataContext();
+             var result = from app in dc.Appointments
+                          join doc in dc.Doctors on app.DoctorID equals doc.DoctorID
+                          join p in dc.Patients on app.PatientID equals p.PatientID
+                          where app.Status == "Ref to Desk"
+                          select new
+                          {
+                              AppointmentID = app.AppointmentID,
+                              DoctorID = doc.DoctorID,
+                              Department = doc.Designation,
+                              DoctorName = doc.FirstName + doc.LastName,
+                              DoctorImage = doc.Image,
+                              DoctorEmail = doc.DoctorEmail,
+                              PatientID = p.PatientID,
+                              PatientName = p.PatientName,
+                              PatientEmail = p.PatientEmail,
+                              PatientContact = p.PatientContact,
+                              PatientImage = p.PatientImage,
+                              Date = app.Date,
+                              TimeSlot = app.TimeSlot,
+                              Status = app.Status
+
+                          };
+
+
+             if (result != null)
+             {
+                 foreach (var v in result)
+                 {
+
+                     AppointmentDetails details = new AppointmentDetails();
+                     details.AppointmentID = v.AppointmentID;
+                     details.DoctorID = v.DoctorID;
+                     details.Designation = v.Department;
+                     details.DoctorName = v.DoctorName;
+                     details.DoctorImage = v.DoctorImage;
+                     details.DoctorEmail = v.DoctorEmail;
+                     details.PatientID = v.PatientID;
+                     details.PatientName = v.PatientName;
+                     details.PatientEmail = v.PatientEmail;
+                     details.PatientContact = v.PatientContact;
+                     details.PatientImage = v.PatientImage;
+                     details.Date = v.Date.ToString();
+                     details.TimeSlot = v.TimeSlot;
+                     AppList.Add(details);
+
+                 }
+                 return AppList;
+             }
+             return AppList;
+         }
+    }
+}
---
+++ ERP_SupplyChain/Controllers/FrontDeskController.cs
@@ -0,0 +1,213 @@
+﻿using DataAccessLayer.DAL_Logic.Appointments;
+using DataAccessLayer.DAL_Logic.FrontDesk;
+using ERPEntities;
+using ERPEntities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_SupplyChain.Controllers
+{
+    public class FrontDeskController : Controller
+    {
+        DoctorDAL Doc = new DoctorDAL();
+        List<AppointmentDetails> details = new List<AppointmentDetails>();
+        ERP1DataContext dc = new ERP1DataContext();
+        //
+        // GET: /FrontDesk/
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+        public ActionResult AddSchedule()
+        {
+            return View();
+        }
+
+        //Get Assign Schedule
+        public ActionResult AssignSchedule()
+        {
+            List<DepartmentVM> Departments = new List<DepartmentVM>();
+            Departments.Add(new DepartmentVM() { Department = "Cardiology", Value = "Cardiology" });
+            Departments.Add(new DepartmentVM() { Department = "Neurology", Value = "Neurology" });
+            Departments.Add(new DepartmentVM() { Department = "Gynecology", Value = "Gynecology" });
+            Departments.Add(new DepartmentVM() { Department = "Gastrology", Value = "Gastrology" });
+            Departments.Add(new DepartmentVM() { Department = "Arthropathy", Value = "Arthropathy" });
+            Departments.Add(new DepartmentVM() { Department = "Dermatology", Value = "Dermatology" });
+
+            ViewBag.Department = new SelectList(Departments, "Value", "Department");
+
+            List<Schedule> ScheduleList = dc.Schedules.ToList();
+            ViewBag.ScheduleList = new SelectList(ScheduleList, "EventID", "Subject");
+            return View();
+        }
+
+        // GET: /Schedule/GetDoctorList
+        public JsonResult GetDoctorList(string department)
+        {
+            //db.Configration.ProxyCreation enabled = false;
+            List<Doctor> DoctorList = dc.Doctors.Where(x => x.Department == department).ToList();
+
+            return Json(DoctorList, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult SaveSchedule(int DoctorID, int EventID)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (ERP1DataContext dc = new ERP1DataContext())
+                {
+
+                    DoctorSchedule ds = new DoctorSchedule();
+                    ds.DoctorID = DoctorID;
+                    ds.EventID = EventID;
+                    dc.DoctorSchedules.InsertOnSubmit(ds);
+                    dc.SubmitChanges();
+                    status = true;
+                }
+            }
+            else
+            {
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        //Get Event
+        public JsonResult GetEvents()
+        {
+            using (ERP1DataContext dc = new ERP1DataContext())
+            {
+                var events = dc.Schedules.ToList();
+
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        //Save Event
+        [HttpPost]
+        public JsonResult SaveEvent(Schedule s)
+        {
+            var status = false;
+            using (ERP1DataContext dc = new ERP1DataContext())
+            {
+                if (s.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Schedules.Where(a => a.EventID == s.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = s.Subject;
+                        v.Start = s.Start;
+                        v.End = s.End;
+                        v.Description = s.Description;
+                        v.IsFullDay = s.IsFullDay;
+                        v.Theme = s.Theme;
+                    }
+                }
+                else
+                {
+                    dc.Schedules.InsertOnSubmit(s);
+                }
+                dc.SubmitChanges();
+                status = true;
+            }
+
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        //Delete Event
+        [HttpPost]
+        public JsonResult deleteEvent(int eventID)
+        {
+            var status = false;
+            using (ERP1DataContext dc = new ERP1DataContext())
+            {
+                var v = dc.Schedules.Where(a => a.EventID == eventID).FirstOrDefault();
+                var data = dc.DoctorSchedules.Where(a => a.EventID == eventID).ToList();
+                if (v != null || data != null)
+                {
+                    foreach (DoctorSchedule ds in data)
+                    {
+                        dc.DoctorSchedules.DeleteOnSubmit(ds);
+                    }
+                    dc.Schedules.DeleteOnSubmit(v);
+                    dc.SubmitChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        public ActionResult ScheduleApp()
+        {
+            FrontDeskDAL Front = new FrontDeskDAL();
+            List<AppointmentDetails> details = Front.getScheduledAppointmentDetails();
+            return View(details);
+        }
+
+        public ActionResult ReferedApp()
+        {
+            FrontDeskDAL Front = new FrontDeskDAL();
+            List<AppointmentDetails> details = Front.getReferedAppointmentDetails();
+            return View(details);
+        }
+
+        public ActionResult GenerateBill(int id)
+        {
+            ViewBag.AppID = id;
+            return View();
+        }
+        public JsonResult AppointmentDetails(int id)
+        {
+            //db.Configration.ProxyCreation enabled = false;
+            List<AppointmentDetails> details = Doc.getAppDetails(id);
+
+            return Json(details, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult SaveBill(PrescriptionVM P)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (ERP1DataContext dc = new ERP1DataContext())
+                {
+                    Prescription pres = new Prescription { AppointmentID = P.AppointmentID, DoctorID = P.DoctorID, PatientID = P.PatientID };
+                    foreach (var i in P.MedDetails)
+                    {
+                        //
+                        // i.TotalAmount = 
+                        pres.Medications.Add(i);
+                    }
+                    dc.Prescriptions.InsertOnSubmit(pres);
+                    dc.SubmitChanges();
+                    var data = dc.Appointments.Where(a => a.AppointmentID == P.AppointmentID).ToList();
+                    foreach (Appointment A in data)
+                    {
+                        //
+                        // i.TotalAmount =
+                        A.Status = "Prescribed";
+                        dc.SubmitChanges();
+
+                    }
+                    dc.SubmitChanges();
+                    status = true;
+                }
+            }
+            else
+            {
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+	}
+}
